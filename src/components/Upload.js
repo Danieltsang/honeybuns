39159c008f3f857cb3c012ae5b3483ca125d5fca
danieltsang <danieltsang94@gmail.com>
2017-03-19 10:21:36 -0700

    css changes

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -7,26 +7,22 @@ class Upload extends Component {
       <div>
         <div className='container-fluid'>
           <div className='row'>
-            <div className='col-xs-offset-4 col-xs-1'>
               <div className='logo-container'>
+                <h1>HONEY POT</h1>
                 <img src={require('../images/hb-logo.png')} alt=''/>
               </div>
-            </div>
-            <div className='col-xs-1'>
-              <h2>HONEY POT</h2>
-            </div>
           </div>
         </div>
         <div className='upload-image-container'>
+          {/*<div>*/}
+            {/*<h4> Android Text Instructions </h4>*/}
+            {/*<div className='image-container'>*/}
+              {/*<img src={require('../images/android-instruction-1.png')} alt=''/>*/}
+              {/*<img src={require('../images/android-instruction-2.png')} alt=''/>*/}
+            {/*</div>*/}
+          {/*</div>*/}
           <div>
-            <h4> Android Text Instructions </h4>
-            <div className='image-container'>
-              <img src={require('../images/android-instruction-1.png')} alt=''/>
-              <img src={require('../images/android-instruction-2.png')} alt=''/>
-            </div>
-          </div>
-          <div>
-            <h4> iPhone Text Instructions </h4>
+            <h4 className="text-instructions"> iPhone Text Instructions </h4>
             <div className='image-container'>
               <img src={require('../images/iphone-instruction-1.jpg')} alt=''/>
               <img src={require('../images/iphone-instruction-2.jpg')} alt=''/>
