8154beb40013b35e6238a9e7f33f2c3cc54f9f1a
Jasdeep Kahlon <jasdeep.kln@gmail.com>
2017-03-18 22:28:50 -0700

    Getting form data and passing it to filter func

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// x : [{
+//   date: "2017-06-05",
+//   name: "Brandon",
+//   msg: "wyd"
+// },
+// {
+//   date: "2017-06-06",
+//   name: "Jason",
+//   msg: "nada"
+// }]
 
 class App extends Component {
+  getFormData(){ 
+    var start=document.getElementById('start').value;
+    var end=document.getElementById('end').value;
+    /* some other fields */
+    /* now call ur function by passing the above values */
+    filterByDateRange(start, end);
+  }
+  filterByDateRange(start, end) {
+      var result = [];
+      var messages = [];
+      for (var i = messages.length - 1; i >= 0; i--) {
+        if (messages[i].date <= end && messages[i].date >= start) {
+          result.add(messages[i]);
+        }
+      }
+      console.log(result);
+      return result;
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +41,17 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <form>
+  <label>
+    start:
+    <input type="date" name="start" />
+  </label>
+  <label>
+    end:
+    <input type="date" name="end" />
+  </label>
+  <input type="submit" value="Submit" onclick="getFormData()" />
+</form>
       </div>
     );
   }
