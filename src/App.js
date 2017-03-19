f1bf5514e85df54fc0e91b073eb573d9a2c290e2
EWaterman <evanwaterman@hotmail.com>
2017-03-18 23:49:47 -0700

    half of top words API

--- src/App.js
+++ src/App.js
@@ -1,37 +1,93 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import '.'
-import Upload from './components/Upload.js'
-import Message from './components/Message.js'
-import Content from './components/Content.js'
-import { Grid, Row, Col } from 'react-bootstrap'
+import '.';
+import Upload from './components/Upload.js';
+import Message from './components/Message.js';
+import Content from './components/Content.js';
+import { Grid, Row, Col } from 'react-bootstrap';
+import Chart from 'chart.js';
+
+//TODO: replace this with parser
+var messages = [
+    {'date':'???', 'user':'Steve', 'message':'I like turtles'},
+    {'date':'???', 'user':'Martin', 'message':'Me too!'},
+    {'date':'???', 'user':'Steve', 'message':'No, like, I REALLY like turtles...'},
+    {'date':'???', 'user':'Martin', 'message':'Okay whatever man.'}
+];
 
 class App extends Component {
+
   constructor(props) {
     super(props);
-  
     this.state = {showMainApp: false};
   }
-  render() {
-    var mainArea;
-    if (this.state.showMainApp) {
-      mainArea = (
-        <Grid>
-        <Row>
-          <Col xs={4} md={4}>
-            <Message />
-          </Col>
-          <Col xs={8} md={8}>
-            <Content />
-          </Col>
-        </Row>
-      </Grid>
-      );
-    } else {
-      mainArea = <Upload />;
+
+  topWords(messages) {
+    //TODO: pick out the top used words from the messages array
+
+    var topWords = ['I', 'like', 'turtles'];
+    var wordCount = {'I': 25, 'like': 10, 'turtles': 5};
+
+    return [topWords, wordCount];
+  }
+
+  renderTopWords(messages) {
+    const BarChart = require("react-chartjs").Bar;
+
+    var temp = this.topWords(messages);
+    var topWords = temp[0];
+    var wordCount = temp[1];
+
+    var labels_arr = [];
+    var data_arr = [];
+    var arrayLength = topWords.length;
+    for (var i = 0; i < arrayLength; i++) {
+      labels_arr.push(topWords[i]);
+      data_arr.push(wordCount[topWords[i]]);
     }
-    return mainArea;
+    var barChartData = {
+      labels: labels_arr,
+      datasets: [{
+        label: 'Most Frequent Words',
+        data: data_arr,
+        borderWidth: 1
+      }]
+    };
+
+    var barChartOptions = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    };
+
+    return(<BarChart data={barChartData} options={barChartOptions} />);
+  }
+
+  render() {
+    // var mainArea;
+    // if (this.state.showMainApp) {
+    //   mainArea = (
+    //     <Grid>
+    //       <Row>
+    //         <Col xs={4} md={4}>
+    //           <Message />
+    //         </Col>
+    //         <Col xs={8} md={8}>
+    //           <Content />
+    //         </Col>
+    //       </Row>
+    //     </Grid>
+    //   );
+    // } else {
+    //   mainArea = <Upload />;
+    // }
+    // return mainArea;
+    return this.renderTopWords();
   }
 }
 
