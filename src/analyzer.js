4db1527e990c7a17471d57f347630be8d8a6bf46
jasonpm <jasonmasih@dhcp-206-87-196-83.ubcsecure.wireless.ubc.ca>
2017-03-19 11:19:30 -0700

    rolling count

--- src/analyzer.js
+++ src/analyzer.js
@@ -120,7 +120,9 @@ Analyzer.prototype.analyze = function(message) {
             averageSentiment: sentiment
         };
         userCount = userCount + 1;
-        console.log(userCount);
+        if (userCount > colors.length-1) {
+            userCount = 0
+        }
     } else {
         this.update(this.data.users[message.name], words, numWords, messageLength, sentiment);
     }
