6520bae00fd06e89611d5d5072b61278acae7930
jasonpm <jasonmasih@dhcp-206-87-196-83.ubcsecure.wireless.ubc.ca>
2017-03-18 22:03:10 -0700

    started making components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '.'
+import Message from './components/Message.js'
+import Content from './components/Content.js'
+import { Grid } from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid>
+        <Row>
+          <Col xs={4} md={4}>
+            <Message />
+          </Col>
+          <Col xs={8} md={8}>
+            <Content />
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
